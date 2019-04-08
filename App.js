78c77c190aa34386ff443c47d50cc2cd912a70b7
nandoplayart <luiz.fernando.desenvolvimento@outlook.com>
2019-04-08 10:23:30 -0300

    4 files modified, 4 added

--- App.js
+++ App.js
@@ -2,7 +2,5 @@ import React, {Fragment } from 'react'
 import Routes from './routes'
 import GlobalStyle from "./global"
 const App =() => <Fragment><GlobalStyle></GlobalStyle><Routes /></Fragment>;
-//const App =() => <h1>Start</h1>;
-
 
 export default App;
--- pages/App/index.js
+++ pages/App/index.js
@@ -1,14 +1,22 @@
-import React,{Component} from 'react';
+import React,{Component,Fragment} from 'react';
 import Dimensions from 'react-dimensions';
-import {Container} from './styles';
+import {Container,ButtonContainer } from './styles';
+import {withRouter} from 'react-router-dom';
+import Button from '../Properties/Button';
 import MapGL from 'react-map-gl';
 import PropTypes from 'prop-types';
+import debounce from 'lodash/debounce';
+import api from '../../services/api';
+import {logout} from '../../services/auth'
+import Properties from '../Properties';
 
 const TOKEN = 'pk.eyJ1IjoibmFuZG9wbGF5YXJ0IiwiYSI6ImNqdTRpa2gyeTB5M2o0NG55Njhsd3RzODIifQ.5VPBYznByJF30pByOK1w5g';
 
 class Map extends Component{
+
+
   static propTypes ={
-    containerWith:PropTypes.number.isRequired,
+    containerWidth:PropTypes.number.isRequired,
     containerHeight:PropTypes.number.isRequired
   }
 
@@ -19,24 +27,63 @@ state ={
     zoom:12.8,
     bearing:0,
     pitch:0
+  },
+   properties:[]
+}
+
+handleLogout = e=>{
+  logout();
+  this.props.history.push('/');
+}
+
+renderActions(){
+  return (
+    <ButtonContainer>
+      <Button color="#222" onClick={this.handleLogout}>
+        <i className="fa fa-times"/>
+      </Button>
+    </ButtonContainer>
+  )
+}
+
+componentDidMount(){
+  this.loadProperties();
+}
+updatePropertiesLocalization(){
+  this.loadProperties();
+}
+
+loadProperties = async() =>{
+  const {latitude, longitude} = this.state.viewport;
+  try{
+    const response = await api.get('/properties',{params:{latitude,longitude}})
+    this.setState({properties:response.data});
+  }catch(err){
+    console.log(err);
   }
 }
 
 
 render(){
   const {containerWidth:width,containerHeight:height} = this.props
+  const {properties} = this.state;
   return (
-    <MapGL width={width} height={height} {...this.state.viewport}
-    mapStyle="mapbox://styles/mapbox/dark-v9"
-    mapboxApiAccessToken={TOKEN}
-    onviewportChange={viewport =>  this.setState({viewport}) }
-     />
+    <Fragment>
+        <MapGL width={width} height={height} {...this.state.viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={TOKEN}
+        onViewportChange={viewport =>  this.setState({viewport})}
+        onViewStateChange={this.updatePropertiesLocalization.bind(this)}>
+        <Properties properties={properties} />
+        </MapGL>
+        {this.renderActions()}
+    </Fragment>
   )
 }
 
 }
 
-const DimensionedMap = Dimensions()(Map);
+const DimensionedMap = withRouter(Dimensions()(Map));
 const App =() =>(
   <Container>
     <DimensionedMap />
